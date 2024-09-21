ac7dbea6de0030bf3f3e2444095424407d241db6
funkyenough <14842981+funkyenough@users.noreply.github.com>
2024-09-21 15:54:27 +0900

    add ChatGPT API

---
+++ poo/ChatGPTRawView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatGPTRawView.swift
+//  poo
+//
+//  Created by Ying Hong on 2024/09/21.
+//
+
+import SwiftUI
+
+struct ChatGPTRawView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatGPTRawView()
+}
