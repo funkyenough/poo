aee64819687708c0021705edc415078993bba885
YAO <nyaria1014@gmail.com>
2024-09-21 11:05:04 +0900

    Initial Commit

---
+++ poo/pooApp.swift
@@ -0,0 +1,17 @@
+//
+//  pooApp.swift
+//  poo
+//
+//  Created by YaoNing on 2024/09/21.
+//
+
+import SwiftUI
+
+@main
+struct pooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
