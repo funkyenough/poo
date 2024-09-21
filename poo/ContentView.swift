a24de41a682aa643b1dfcb563be81d47b451b40c
YAO <nyaria1014@gmail.com>
2024-09-21 11:35:22 +0900

    11

--- poo/ContentView.swift
+++ poo/ContentView.swift
@@ -20,5 +20,81 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    LoginView()
+}
+
+
+
+struct LoginView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var showPopup = false
+
+    var body: some View {
+        ZStack {
+            VStack {
+                Text("Login")
+                    .font(.largeTitle)
+                    .padding()
+
+                TextField("Username", text: $username)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+
+                SecureField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+
+                Button(action: {
+                    // Perform login action here
+                    print("Login tapped")
+                    showPopup = true
+                }) {
+                    Text("Log In")
+                        .foregroundColor(.white)
+                        .frame(width: 200, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+            }
+            .padding()
+
+            if showPopup {
+                Color.black.opacity(0.4)
+                    .edgesIgnoringSafeArea(.all)
+                    .onTapGesture {
+                        showPopup = false
+                    }
+
+                VStack {
+                    Image(systemName: "person.circle.fill")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(.blue)
+
+                    Text("Welcome, \(username)!")
+                        .font(.title)
+                        .padding()
+
+                    Button("Close") {
+                        showPopup = false
+                    }
+                    .padding()
+                }
+                .frame(width: 300, height: 300)
+                .background(Color.white)
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .transition(.scale)
+                .animation(.spring())
+            }
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
 }
