d38fca61b7cde3f38281ba8f3017a6ffc7da2a9c
YAO <nyaria1014@gmail.com>
2024-09-22 11:57:37 +0900

    login page

--- poo/pooApp.swift
+++ poo/pooApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct pooApp: App {
     var body: some Scene {
         WindowGroup {
-            MenuBar()
+            LoginView()
                 .environmentObject(PooViewModel())
         }
     }
